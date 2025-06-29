77bca0f2a5ba533f64a6a7af4efc4ff4398c6676
Mohamed Hosny <mohammedhosny53@gmail.com>
2025-06-29 18:34:19 +0300

    edit in fruit details

--- lib/features/home/domain/repositories/product_repository_impl.dart
+++ lib/features/home/domain/repositories/product_repository_impl.dart
@@ -7,17 +7,17 @@ import 'package:fruit_hub/features/home/domain/models/product_model.dart';
 import 'package:fruit_hub/features/home/domain/repositories/product_repository.dart';
 
 class ProductRepositoryImpl implements ProductRepository {
-  final ProductRemoteDataSource remoteDataSource;
+  final ProductRemoteDataSource? remoteDataSource;
   final ApiHelper apiHelper;
 
   ProductRepositoryImpl({
-    required this.remoteDataSource,
+    this.remoteDataSource,
     required this.apiHelper,
   });
 
   @override
   Future<ProductsResponse> getRecommendedCombos() {
-    return remoteDataSource.getRecommendedCombos();
+    return remoteDataSource!.getRecommendedCombos();
   }
 
   @override
--- lib/features/home/presentation/views/fruit_details_view.dart
+++ lib/features/home/presentation/views/fruit_details_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub/core/network/api_helper.dart';
 import 'package:fruit_hub/features/home/domain/repositories/product_repository.dart';
+import 'package:fruit_hub/features/home/domain/repositories/product_repository_impl.dart';
 import 'package:fruit_hub/features/home/presentation/manger/product_details/product_details_cubit.dart';
 import 'package:fruit_hub/features/home/presentation/views/widgets/fruit_details_view_body.dart';
 
@@ -15,9 +17,7 @@ class FruitDetailsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create:
-          (context) => ProductDetailsCubit(
-            productRepository: context.read<ProductRepository>(),
-          )..getProductDetails(productId),
+          (context) => ProductDetailsCubit(productRepository: ProductRepositoryImpl(apiHelper: ApiHelper()))..getProductDetails(productId),
       child: Scaffold(body: ProductDetailsViewBody()),
     );
   }
